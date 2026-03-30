23ce712e8c050cbe693a5f760d28099f8cee9268
ElDynamo <thedynamo2001@gmail.com>
2026-03-30 18:44:13 +0200

    fix(website): precise map coords from client link

--- src/components/forms/Map.vue
+++ src/components/forms/Map.vue
@@ -19,7 +19,7 @@ onMounted(async () => {
   const defaultIcon = L.icon({ iconUrl, iconRetinaUrl, shadowUrl, iconSize: [25, 41], iconAnchor: [12, 41], popupAnchor: [1, -34] });
   L.Marker.prototype.options.icon = defaultIcon;
 
-  map = L.map(mapContainer.value, { scrollWheelZoom: false }).setView([51.1396, 16.9248], 15);
+  map = L.map(mapContainer.value, { scrollWheelZoom: false }).setView([51.1147569, 17.0245063], 15);
 
   L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
@@ -27,7 +27,7 @@ onMounted(async () => {
   }).addTo(map as any);
 
   // Marker — Wrocław, ul. Szkolna 7bh
-  L.marker([51.1396, 16.9248])
+  L.marker([51.1147569, 17.0245063])
     .addTo(map as any)
     .bindPopup(`
       <strong style="font-family:serif;color:#1a3a5c;">Kancelaria Prawna ABW</strong><br/>
